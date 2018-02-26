6607b6a51c6347091b70f41de66cc763e6bb7066
NoseCrater2 <31606925+NoseCrater2@users.noreply.github.com>
2018-02-25 20:36:55 -0600

    Compuerta or
    Pequeño programa que simula el funcionamiento de una compuerta or

---
+++ compuerta or.py
@@ -0,0 +1,7 @@
+class Compuertas:
+    def comp_and(x1,x2):
+        if (-10+20*x1+20*x2)<=0:
+            return 0
+        else:
+            return 1
+#Para llamar al método directamente es Compuertas.comp_and(valor1,valor2) #
